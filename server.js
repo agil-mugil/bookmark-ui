fc23e8be2cc655a4c98435db0c3a49441ecf3b64
pmurugesan15 <pmurugesan2012@gmail.com>
2020-09-17 12:57:09 +0530

    fix: fixing the app not up in heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '/dist/bookmark-ui')));
 
 // Set our api routes proxy to point to spring boot server
-app.use('/server', proxy('http://localhost:8080'));
+app.use('/server', proxy('https://managebookmark.herokuapp.com'));
 
 // Catch all other routes and return the index file
 app.get('/*', (req, res) => {
@@ -28,7 +28,7 @@ app.get('/*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
- const port = '4200';
+ const port = '8080';
 app.set('port', port);
 
 /**
